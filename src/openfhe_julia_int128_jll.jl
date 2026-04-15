591aa5379baddd07f64ba4148bb2d13fcb89279f
jlbuild <juliabuildbot@gmail.com>
2026-04-15 16:10:46 +0000

    openfhe_julia_int128_jll build 0.6.0+0

--- src/openfhe_julia_int128_jll.jl
+++ src/openfhe_julia_int128_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("openfhe_julia_int128")
-JLLWrappers.@generate_main_file("openfhe_julia_int128", UUID("328aa5c7-aa14-5b23-aa57-05fb7d04b830"))
+JLLWrappers.@generate_main_file("openfhe_julia_int128", Base.UUID("328aa5c7-aa14-5b23-aa57-05fb7d04b830"))
 end  # module openfhe_julia_int128_jll
